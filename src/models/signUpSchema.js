c3d0378c52728d71602006f4dc61d8092903ddc3
Lucas <lucasreis64@gmail.com>
2023-01-04 17:45:45 -0300

    fix: signUpSchema

--- src/models/signUpSchema.js
+++ src/models/signUpSchema.js
@@ -1,10 +1,10 @@
 import Joi from "joi";
 
 export const signUpSchema = Joi.object({
-    name: Joi.string().min(3).max(30).required(),
+    username: Joi.string().min(3).max(15).required(),
     email: Joi.string()
         .email({ minDomainSegments: 2, tlds: { allow: ["com", "net", "br"] } })
         .required(),
     password: Joi.string().min(5).required(),
-    confirmPassword: Joi.ref("password"),
+    url: Joi.string().uri().required()
 });
