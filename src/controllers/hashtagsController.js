0a0771914170f12b8c09f6a072f76cb53c383035
unknown <jose.filipe.toledo.1999@gmail.com>
2023-01-05 13:30:20 -0300

    trendings prontas

---
+++ src/controllers/hashtagsController.js
@@ -0,0 +1,13 @@
+import { connection } from "../db/database.js";
+
+export async function rankTrendings(req, res){
+    try {
+        const rank = await connection.query(`
+        SELECT * FROM "hashtags" ORDER BY users DESC LIMIT 10;`);
+
+        return res.status(200).send(rank.rows);
+    } catch (err){
+        console.log(err);
+        return res.sendStatus(500);
+    }
+}
