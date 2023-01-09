d3698a344052e6675895c7c6d6b032964936ccd0
jhuvis <jose.filipe.toledo.1999@gmail.com>
2023-01-09 15:07:12 -0300

    arrumando #

--- src/routers/hashtagsRouter.js
+++ src/routers/hashtagsRouter.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { rankTrendings, getByHashtag } from '../controllers/hashtagsController.js';
+import  validateToken  from '../middlewares/validateToken.js';
 
 const router = express.Router();
 
 router.get("/trendings", rankTrendings);
-router.get("/hashtag/:hashtag", getByHashtag);
+router.get("/hashtag/:hashtag", validateToken, getByHashtag);
 
 export default router;
