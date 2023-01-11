4b9d9e3b83b1f0fe3211c42d9e7ce9a725fe9582
vsisterolli <vsisterolli@gmail.com>
2023-01-11 19:08:53 -0300

    feat: follow & prioritize them

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,5 +1,19 @@
-import { searchUser, queryUserById, queryUserData } from "../repositories/usersRepository.js";
+import { searchUser, queryUserById, queryUserData, followUser, unfollowUser } from "../repositories/usersRepository.js";
 import { metadata } from "../services/getMetadataByLink.js";
+
+export async function postFollow(req, res) {
+    const { followedId } = req.params;
+    if(followedId == res.locals.user.id)
+        return res.sendStatus(401);
+    return res.sendStatus(await followUser(followedId, res.locals.user.id))
+}
+
+export async function deleteFollow(req, res) {
+    const { followedId } = req.params;
+    if(followedId == res.locals.user.id)
+        return res.sendStatus(401);
+    return res.sendStatus(await unfollowUser(followedId, res.locals.user.id))
+}
 
 export async function searchUsers(req, res) {
     
@@ -7,8 +21,7 @@ export async function searchUsers(req, res) {
         const name = req.query.name;
         if(!name || name.lenght < 3)
             return res.sendStatus(422);
-
-        const usersSuggestion = await searchUser(name);
+        const usersSuggestion = await searchUser(name, res.locals.user.id);
         return res.status(200).send(usersSuggestion);     
     }
     catch(e) {
@@ -21,13 +34,15 @@ export async function searchUsers(req, res) {
 export async function getUserData(req, res) {
     try {
         const { id } = req.params;
-
+        const userRequesting = res.locals.user;
+        
         const userExist = await queryUserById(id);
         if(!userExist.length)
             return res.sendStatus(404)
 
-        const posts = await queryUserData(id);
-        
+        const posts = await queryUserData(id, userRequesting.id);
+        posts.is_following = (posts.is_following === '0' ? false : true)
+
         for(let i = 0; i < posts.user_posts.length; i++) 
             posts.user_posts[i]["link_metadata"] = await metadata(posts.user_posts[i].link)
         
